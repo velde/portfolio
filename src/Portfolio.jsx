7a96d1c52d09a5b766692fa4aaece2169b6b513a
velde <vvainio@gmail.com>
2025-02-26 21:52:10 +0200

    Initial commit for portfolio

---
+++ src/Portfolio.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Portfolio() {
+  return (
+    <div style={{ background: "lightblue", height: "100vh", textAlign: "center", paddingTop: "50px" }}>
+      <h1>Hello, My Portfolio is Working!</h1>
+    </div>
+  );
+}
